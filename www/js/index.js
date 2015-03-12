3137d3488581fd2919268dafcf8d869ae35eb5b7
Babis Boikos <xboikos@gmail.com>
2015-03-12 18:51:02 +0100

    Not easy to create pdf

--- www/js/index.js
+++ www/js/index.js
@@ -679,6 +679,56 @@ AGENTI.offerta = {
 
     inviaOfferta: function () {
 
+        //FIRST GENERATE THE PDF DOCUMENT
+        console.log("generating pdf...");
+        var doc = new jsPDF();
+
+        doc.text(20, 20, 'HELLO!');
+
+        doc.setFont("courier");
+        doc.setFontType("normal");
+        doc.text(20, 30, 'This is a PDF document generated using JSPDF.');
+        doc.text(20, 50, 'YES, Inside of PhoneGap!');
+
+        var pdfOutput = doc.output();
+        console.log( pdfOutput );
+
+        //NEXT SAVE IT TO THE DEVICE'S LOCAL FILE SYSTEM
+        console.log("file system...");
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+
+                console.log(fileSystem.name);
+                console.log(fileSystem.root.name);
+                console.log(fileSystem.root.fullPath);
+
+                fileSystem.root.getFile("test.pdf", {create: true}, function(entry) {
+                    var fileEntry = entry;
+                    console.log(entry);
+
+                    entry.createWriter(function(writer) {
+                        writer.onwrite = function(evt) {
+                            console.log("write success");
+                        };
+
+                        console.log("writing to file");
+                        writer.write( pdfOutput );
+                    }, function(error) {
+                        console.log(error);
+                    });
+
+                }, function(error){
+                    console.log(error);
+                });
+            },
+            function(event){
+                console.log( evt.target.error.code );
+            });
+
+
+
+
+
+
         /* cordova.plugins.email.isAvailable(
          function (isAvailable) {
          // alert('Service is not available') unless isAvailable;
@@ -689,6 +739,7 @@ AGENTI.offerta = {
             emailProperties = { to: AGENTI.client.email,
                 cc: 'g.marra@siderprofessional.com',
                 subject: 'Offerta Sidercampania Professional srl ',
+                attachments: 'file:///storage/sdcard/test.pdf',
                 isHtml:  true};
 
 
