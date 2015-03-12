e867083eb65358aadfcdad08a2d1664d2df6193f
Babis Boikos <xboikos@gmail.com>
2015-03-12 15:33:11 +0100

    Plugin hell resolved?

--- hooks/after_platform_add/010_install_plugins.js
+++ hooks/after_platform_add/010_install_plugins.js
@@ -20,7 +20,7 @@ var pluginlist = [
 
 var fs = require('fs');
 var path = require('path');
-var sys = require('sys')
+var sys = require('sys');
 var exec = require('child_process').exec;
 
 function puts(error, stdout, stderr) {
