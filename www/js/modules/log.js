746b15ad8f62572e09159b9814f97b3a3450e867
Babis <xboikos@gmail.com>
2015-04-13 00:03:35 +0200

    Modularizing

--- www/js/modules/log.js
+++ www/js/modules/log.js
@@ -1,27 +1,33 @@
 /**
  * Created by Babis on 02/04/2015.
  */
+AGENTI.log = (function () {
 
-AGENTI.log.getLog = function () {
-    /*Variable declaration*******************/
-    var that = AGENTI.log,
-        userName = AGENTI.db.getItem('username'),
+    var getLog = function () {
+        /*Variable declaration*******************/
+        var userName = AGENTI.db.getItem('username'),
+            queryData;
+        /*End of variable declaration************/
+
         queryData = {action: 'getLog', user: userName};
-    /*End of variable declaration************/
+        //get data from the server
+        AGENTI.getData(queryData, renderLog);
 
-    //get data from the server
-    AGENTI.getData(queryData, that.renderLog);
+    };
 
-};
+    var renderLog = function (result) {
+        /*Variable declaration*******************/
+        var html = "";
+        /*End of variable declaration************/
 
-AGENTI.log.renderLog = function (result) {
-    /*Variable declaration*******************/
-    var html = "";
-    /*End of variable declaration************/
+        $.each(result, function () {
+            html += '<li><p style="font-size:x-small;">' + this.logTimestamp + ' ' + this.logIp + '</p><p style="font-size:x-small;">' + this.logDescr + '</p></li>';
+        });
 
-    $.each(result, function () {
-        html += '<li><p style="font-size:x-small;">' + this.logTimestamp + ' ' + this.logIp + '</p><p style="font-size:x-small;">' + this.logDescr + '</p></li>';
-    });
+        $('#log').html(html).listview("refresh");
+    };
 
-    $('#log').html(html).listview("refresh");
-};
+    return {
+        getLog: getLog
+    }
+})();
