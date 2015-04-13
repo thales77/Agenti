5f186effc77bfee93c45fc9dfe3f308000393006
Babis Boikos <xboikos@gmail.com>
2015-04-13 19:00:34 +0200

    Modularizing

--- www/js/modules/utils.js
+++ www/js/modules/utils.js
@@ -2,50 +2,71 @@
  * Created by Babis on 02/04/2015.
  */
 
-AGENTI.utils = {
+AGENTI.utils = (function () {
+
+//Function to handle pagination in long lists
+    var pagination = function () {
+        //Private variables
+        var _noRecordsPerPage = 20,
+            _offset = 0;
+
+        //Public methods
+        var addOffset = function (listOffset) {
+            _offset = _offset + parseInt(listOffset, 10);
+        };
+
+        var resetOffset = function () {
+            _offset = 0;
+        };
+
+        var getOffset = function () {
+            return _offset;
+        };
+
+        var setRecordsPerPage = function (recordsPerPage) {
+            _noRecordsPerPage = parseInt(recordsPerPage, 10);
+        };
+
+        var getProperties = function () {
+            return {
+                listOffset: _offset,
+                perPage: _noRecordsPerPage
+            };
+        };
+        return {
+            addOffset: addOffset,
+            resetOffset: resetOffset,
+            getOffset: getOffset,
+            setRecordsPerPage: setRecordsPerPage,
+            getProperties: getProperties
+        }
+    };
+
+
     //Open the map in native maps app
-    //add blackberry support?
-    openMap: function (address) {
+    var openMap = function (address) {
         if (AGENTI.deviceType === 'Android') {
             window.location = "geo:0,0?q=" + address;
         } else {
             window.location = "maps:q=" + address;
         }
-    },
-    vibrate: function (){
+    };
+
+    //wiggle it
+    var vibrate = function () {
         //shake it baby
         if (AGENTI.deviceType === 'Android') {
             //this doesn't work well in ios
             navigator.notification.vibrate(10);
         }
-    }
-
-};
-
-//Pagination module
-AGENTI.utils.pagination = (function () {
-    //Private variables
-    var noRecordsPerPage = 20,
-        offset = 0;
-    return {
-        //Public methods
-        addOffset: function (listOffset) {
-            offset = offset + parseInt(listOffset, 10);
-        },
-        resetOffset: function () {
-            offset = 0;
-        },
-        getOffset: function () {
-            return offset;
-        },
-        setRecordsPerPage: function (recordsPerPage) {
-            noRecordsPerPage = parseInt(recordsPerPage, 10);
-        },
-        getProperties: function () {
-            return {
-                listOffset: offset,
-                perPage: noRecordsPerPage
-            };
-        }
     };
+
+
+    //exports
+    return {
+        pagination: pagination,
+        openMap: openMap,
+        vibrate: vibrate
+    };
+
 })();
